b230f60cee6b600371f042a2eebfb61207e28ed9
Jesse MacFadyen <purplecabbage@gmail.com>
2017-05-18 01:30:46 +0200

    piano pitched

--- HTMLPiano/www/index.js
+++ HTMLPiano/www/index.js
@@ -3,22 +3,65 @@
 document.addEventListener('DOMContentLoaded',onLoad);
 
 var AudioContext = window.AudioContext || window.webkitAudioContext;
+var piano;
+var padHitEventName = "mousedown";
+var padReleaseEventName = "mouseup";
 
 function onLoad() {
+
+    try {
+        document.createEvent("TouchEvent");
+        padHitEventName = "touchstart";
+        padReleaseEventName = "touchend";
+    }
+    catch(ex) {
+        // alert('Touch is not supported in this browser');
+    }
+
     if(!AudioContext) {
         window.alert("WebAudio is not supported");
     }
     else {
-        var audioCtx = new AudioContext();
-        var oscillator = audioCtx.createOscillator();
-        oscillator.type = 'sine';
-        // sine wave — other values are 'square', 'sawtooth', 'triangle' and 'custom'
-        oscillator.frequency.value = 2500; // value in hertz
-        oscillator.start(0);
+        piano = new TunedInstrument();
+        piano.loadVoice("sounds/AccGuitar.wav",48);
 
-        // var gainNode = audioCtx.createGain();
+        // var audioCtx = new AudioContext();
+        // var oscillator = audioCtx.createOscillator();
+        // oscillator.type = 'sine';
+        // // sine wave — other values are 'square', 'sawtooth', 'triangle' and 'custom'
+        // oscillator.frequency.value = 2500; // value in hertz
+        // // oscillator.start(0);
 
-        // oscillator.connect(gainNode);
-        // gainNode.connect(audioCtx.destination);
+        // // var gainNode = audioCtx.createGain();
+
+        // // oscillator.connect(gainNode);
+        // // gainNode.connect(audioCtx.destination);
+
+        var elems = document.querySelectorAll('.key');
+        elems.forEach( function(elem){
+            //console.log(elem.dataset.notenumber);
+
+            elem.addEventListener(padHitEventName,onPianoKeyDown);
+            if(padHitEventName !== 'mouseup') {
+                elem.addEventListener(padReleaseEventName,onPianoKeyUp);
+            }
+        });
     }
+}
+
+function onPianoKeyDown(evt) {
+    if(evt.type == "mousedown") {
+        evt.currentTarget.addEventListener('mouseup',onPianoKeyUp);
+    }
+    piano.noteOn(parseInt(evt.currentTarget.dataset.notenumber) + 24);
+
+}
+
+function onPianoKeyUp(evt) {
+    if(evt.type == "mouseup") {
+        evt.currentTarget.removeEventListener('mouseup',onPianoKeyUp);
+    }
+
+    piano.noteOff(parseInt(evt.currentTarget.dataset.notenumber) + 24);
+    //console.log(evt.currentTarget.dataset.notenumber);
 }
